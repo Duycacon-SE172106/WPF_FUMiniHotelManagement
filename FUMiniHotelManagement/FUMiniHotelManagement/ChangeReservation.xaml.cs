5b20cad3df9140079d8304cbbae7d5ae98310820
Duycacon-SE172106 <156079888+Duycacon-SE172106@users.noreply.github.com>
2024-07-20 04:32:20 +0700

    comit

---
+++ FUMiniHotelManagement/FUMiniHotelManagement/ChangeReservation.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FUMiniHotelManagement
+{
+    /// <summary>
+    /// Interaction logic for ChangeReservation.xaml
+    /// </summary>
+    public partial class ChangeReservation : Window
+    {
+        private readonly IBookingReservationRepositories _bookingReservationRepositories;
+
+        public ChangeReservation()
+        {
+            InitializeComponent();
+            _bookingReservationRepositories = new BookingReservationRepositories();
+        }
+    }
+}
